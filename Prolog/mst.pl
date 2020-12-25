18bfb08a58ab0a60d69923c0cfb76c5d77d4fe3e
Edoardo Fava <Edoardo.F@hotmail.com>
2020-12-25 16:21:12 +0100

    new_arc check graph before checking weight

--- Prolog/mst.pl
+++ Prolog/mst.pl
@@ -19,12 +19,12 @@ new_vertex(G, V) :- graph(G), assert(vertex(G, V)).
 
 new_arc(G, U, V, W) :- graph(G), vertex(G, V), vertex(G, U),
     check_arc(G, U, V, W), !.
-new_arc(G, U, V, W) :- number(W), W >= 0,
-    graph(G), vertex(G, V), vertex(G, U),
+new_arc(G, U, V, W) :- graph(G), number(W), W >= 0,
+    vertex(G, V), vertex(G, U),
     check_arc(G, U, V, W2), !,
     retract_arc(G, U, V, W2), assert_arc(G, U, V, W).
-new_arc(G, U, V, W) :- number(W), W >= 0,
-    graph(G), vertex(G, V), vertex(G, U),
+new_arc(G, U, V, W) :- graph(G), number(W), W >= 0,
+    vertex(G, V), vertex(G, U),
     assert_arc(G, U, V, W).
 new_arc(G, U, V) :- new_arc(G, U, V, 1).
 
